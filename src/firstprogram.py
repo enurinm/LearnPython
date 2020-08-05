326a974582aabad717b1373f6c1a74b826dbacb7
ENU <enurinm@gmail.com>
2020-08-05 15:44:38 +0900

    [COMMIT] learn python with pygame, sys
    learn python with pygame, sys

---
+++ src/firstprogram.py
@@ -0,0 +1,28 @@
+import pygame, sys
+
+from pygame.locals import*
+
+pygame.init()
+pygame.display.set_caption("First Program")
+screen=pygame.display.set_mode((640,480))
+xpos=50
+clock=pygame.time.Clock()
+
+while 1:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            sys.exit()
+            pass
+        pass
+    pressed_keys=pygame.key.get_pressed()
+    if pressed_keys[K_RIGHT]:
+        xpos+=5
+        pass
+    if pressed_keys[K_LEFT]:
+        xpos-=5
+        pass
+    screen.fill((255,255,255))
+    pygame.draw.circle(screen,(0,255,0),(xpos,200),20)
+    pygame.display.update()
+    pass
